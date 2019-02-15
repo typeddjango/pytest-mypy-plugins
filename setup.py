4ddb4a196ca970cba2bb8a39439a71598d704026
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-02-15 19:38:01 +0300

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='pytest-mypy-plugins',
-    version='0.1.0',
+    version='0.1.1',
     description='pytest plugin for writing tests for mypy plugins',
     long_description=readme,
     long_description_content_type='text/markdown',
