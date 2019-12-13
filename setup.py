874c02f97c368ec3807d9ace0d72fdcf084ce40c
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-12-13 11:36:18 +0300

    bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dependencies = [
 
 setup(
     name='pytest-mypy-plugins',
-    version='1.1.0',
+    version='1.2.0',
     description='pytest plugin for writing tests for mypy plugins',
     long_description=readme,
     long_description_content_type='text/markdown',
